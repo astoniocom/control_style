01a98052365e2c61b836bdf537a34db546ca79e5
Viktar Katlianik <7532425@gmail.com>
2023-11-04 08:45:28 +0100

    refactor: place the code in src folder

--- lib/control_style.dart
+++ lib/control_style.dart
@@ -4,8 +4,8 @@
 /// Live example [astoniocom.github.io](https://astoniocom.github.io/control_style/)
 library control_style;
 
-export 'decorated_input_border.dart';
-export 'decorated_outlined_border.dart';
-export 'decoration_painter.dart';
-export 'gradient_border_side.dart';
-export 'gradient_shadow.dart';
+export './src/decorated_input_border.dart';
+export './src/decorated_outlined_border.dart';
+export './src/decoration_painter.dart';
+export './src/gradient_border_side.dart';
+export './src/gradient_shadow.dart';
